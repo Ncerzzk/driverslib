a682f408f3de26bf56db9c994a19a3794459ada5
ncerzzk <huangzzk@bupt.edu.cn>
2020-12-05 15:24:47 +0800

    update the declaration of some functions in spi_slave.c

--- spi_slave.h
+++ spi_slave.h
@@ -12,5 +12,7 @@ typedef struct{
 
 void SPI_Slave_Init(SPI_HandleTypeDef * hspi,uint16_t reg_list_len);
 void SPI_Slave_CSN_Handler(uint8_t flag);
-
+void SPI_COM_Master_Init(SPI_HandleTypeDef * master_hspi,GPIO_TypeDef* csn_port, uint16_t csn_pin,void (*delay_us_func) (int));
+void SPI_COM_Master_Read_Reg(uint8_t reg_addr,uint8_t * reg_data,uint16_t size);
+void SPI_COM_Master_Write_Reg(uint8_t reg_addr,uint8_t * reg_data,uint16_t size);
 #endif
